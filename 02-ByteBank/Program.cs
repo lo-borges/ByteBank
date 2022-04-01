5b66ac838415090b364f562a0bee1a60aee41006
Lorena Borges <lobcborges@gmail.com>
2022-04-01 15:59:55 -0300

    refs e metodos

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaLorena = new ContaCorrente();
+            contaDaLorena.Titular = new Cliente();
+
+            contaDaLorena.Titular.Nome = "Lorena";
+
+            Console.WriteLine("Essa conta pertence a: " + contaDaLorena.Titular.Nome);
+            Console.WriteLine("O seu saldo é de: " + contaDaLorena.Saldo);
+
+            contaDaLorena.Depositar(150);
+            Console.WriteLine("Seu novo saldo, após um depósito é de: " + contaDaLorena.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
