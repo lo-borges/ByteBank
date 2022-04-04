29bfbf7c6e7cda3fae7b42c55e570173f33f9a78
Lorena Borges <lobcborges@gmail.com>
2022-04-04 11:29:22 -0300

    testando construtores

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(001, 2022987001);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.ReadLine();
+        }
+    }
+}
